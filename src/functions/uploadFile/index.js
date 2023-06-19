69301d16b397f8076d00a01ae80d9a249a43c7b3
mughees605 <muhammadmughees08@gmail.com>
2023-06-19 19:20:46 +0500

    first commit

---
+++ src/functions/uploadFile/index.js
@@ -0,0 +1,35 @@
+const AWS = require('aws-sdk');
+const dayjs = require('dayjs');
+
+module.exports.handler = async (event) => {
+  console.log("event", event);
+  const { fileName , fileType} = JSON.parse(event.body);
+  const s3 = new AWS.S3();
+  // const randomID = parseInt(Math.random() * 10000000)
+  // const Key = `${randomID}`;
+
+  const params = {
+    Bucket: `upload-bucket2/${fileType.includes("image") ? 'Images' : 'Videos'}`,
+    Key: `${dayjs().unix()}-${fileName}`,
+    Expires: 1800, // Set the presigned URL expiry time to 30 minutes (in seconds)
+    ContentType: fileType //'image/jpeg'
+  }
+  console.log("params", params);
+  try {
+    const presignedUrl = await s3.getSignedUrlPromise('putObject', params);
+    console.log("presignedUrl", presignedUrl);
+    return {
+      statusCode: 200,
+      body: JSON.stringify({
+        uploadUrl: presignedUrl
+      })
+    };
+  } catch (error) {
+    return {
+      statusCode: 500,
+      body: JSON.stringify({
+        message: error.message
+      })
+    }
+  }
+};
